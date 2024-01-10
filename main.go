76fb27902b6a460d5c93c11fdde5be5fa8b64673
alexs <alexshpita25@gmail.com>
2024-01-10 20:11:38 +0600

    example

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+      "fmt"
+      "database/sql"
+      _ "github.com/go-sql-driver/mysql"
+    )
+
+func main () {
+
+  db, err := sql.Open ("mysql", "root:@tcp(127.0.0.1:3306)/golang")
+  if err != nil {
+    panic(err)
+  }
+
+  defer db.Close ()
+  fmt.Println ("Подключено к MySQL")
+}
