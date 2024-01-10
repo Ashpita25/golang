b1a1655aa4da44eb74ce561f95eec47f0fb6eba9
alexs <alexshpita25@gmail.com>
2024-01-10 21:41:21 +0600

    example

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Ashpita25/golang.git
+
+go 1.21.5
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
       "database/sql"
       _ "github.com/go-sql-driver/mysql"
     )
+type User struct {
+    Name string `json:"name"`
+    Age uint16 `json:'age'`
 
+}
 func main () {
 
   db, err := sql.Open ("mysql", "root:@tcp(127.0.0.1:3306)/golang")
@@ -13,6 +17,28 @@ func main () {
     panic(err)
   }
 
-  defer db.Close ()
-  fmt.Println ("Подключено к MySQL")
+
+  // Установка данных
+ // insert, err := db.Query ("INSERT INTO `users`(`name`, `age`) VALUES ('Kieran', 9)")
+  //if err != nil{
+   // panic (err)
+    //}
+    //defer insert.Close()
+//Выборка данных
+    res, err := db.Query ("SELECT `name`, `age` FROM `users`")
+    if err != nil {
+    panic (err)
+    }
+    for res.Next() {
+    var user User
+    err = res.Scan(&user.Name, &user.Age)
+       if err != nil {
+        panic (err)
+
+
+        }
+        fmt.Println (fmt.Sprintf ("User: %s with  age %d", user.Name, user.Age))
+    }
+defer db.Close ()
 }
+
