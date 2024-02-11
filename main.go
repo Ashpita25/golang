a4dbd3c582bec86ba81f7a94476623e7645ffba1
alexs <alexshpita25@gmail.com>
2024-02-11 11:59:22 +0600

    форма добавления юзера

--- main.go
+++ main.go
@@ -1,34 +1,54 @@
 package main
 
-import (
-      "fmt"
-      //"database/sql"
-      "net/http"
-      "html/template"
-      //_ "github.com/go-sql-driver/mysql"
-    )
+import ("fmt"
+    "net/http"
+    "html/template"
+    "database/sql"
+     _ "github.com/go-sql-driver/mysql")
 
-    func create (w http.ResponseWriter, r *http.Request) {
-            t, err := template.ParseFiles ("templates/index.html", "templates/header.html", "templates/footer.html")
-            if err!= nil {
-            fmt.Println (w, err.Error())
-            }
-            t.ExecuteTemplate (w, "index", nil)
-    }
+func index(w http.ResponseWriter, r *http.Request){
+        t, err :=  template.ParseFiles ("templates/index.html", "templates/header.html", "templates/footer.html")
 
-func index (w http.ResponseWriter, r *http.Request) {
-        t, err := template.ParseFiles ("templates/index.html", "templates/header.html", "templates/footer.html")
-        if err!= nil {
-        fmt.Println (w, err.Error())
+        if err != nil {
+        fmt.Fprintf (w, err.Error())
         }
-        t.ExecuteTemplate (w, "index", nil)
+         t.ExecuteTemplate(w, "index", nil)
 }
-func handleFunc () {
-    http.HandleFunc ("/", index)
+func create(w http.ResponseWriter, r *http.Request){
+        t, err :=  template.ParseFiles ("templates/create.html", "templates/header.html", "templates/footer.html")
+
+        if err != nil {
+        fmt.Fprintf (w, err.Error())
+        }
+         t.ExecuteTemplate(w, "create", nil)
+}
+func save_user(w http.ResponseWriter, r *http.Request){
+        user := r.FormValue("name")
+        pass := r.FormValue("pass")
+        age := r.FormValue("age")
+        db, err := sql.Open ("mysql", "root:@tcp(127.0.0.1:3306)/golang") //подключение к БД
+          if err != nil {
+            panic(err)
+          }
+        defer db.Close ()
+          //Добавление данных
+          insert, err := db.Query (fmt.Sprintf("INSERT INTO `userDB`(`name`,`pass`, `age`) VALUES ('%s','%s','%s')", user, pass, age))
+          if err != nil{
+            panic (err)
+            }
+            defer insert.Close()
+            http.Redirect (w,r,"/", http.StatusSeeOther)
+
+}
+
+func handleRequest() {
+    http.HandleFunc("/", index)
     http.HandleFunc ("/create", create)
-    http.ListenAndServe (":8080", nil)
+    http.HandleFunc ("/save_user", save_user)
+    http.ListenAndServe(":8080", nil)
 }
 func main () {
-        handleFunc ()
-}
+    //bob := User {name: "Bob", age:25, money:-50, avg_grades: 4.2, hapiness: 0.8}
 
+    handleRequest()
+}
